79f0eab9774f971788b90fb5e107e263db64ace8
Priyadharsini29998 <58463585+Priyadharsini29998@users.noreply.github.com>
2019-12-20 14:51:56 +0530

    Add files via upload

---
+++ ValueFinder.java
@@ -0,0 +1,12 @@
+public class ValueFinder {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if (x > 10 && y  > 10) {
+            System.out.println("Condition passed");
+        }
+		else{
+        System.out.println("Condition failed");
+		}
+    }
+}
